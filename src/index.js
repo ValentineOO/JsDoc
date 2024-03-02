503bbe338b77fdd4609a53d5fe9a71447a6e7035
ValentineOO <varlomxg@gmail.com>
2024-03-02 16:46:09 +0100

    numbers and boolean

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// @ts-check
+
+/**
+ * Student Name
+ * @type {string}
+ */
+
+const studentName = "Valentine OO";
+
+/**
+ * Array of grades
+ * @type {Array<number>}
+ */
+
+const grades = [90, 80.7, 100, 70, 85];
+
+/**
+ * @type {{id: number|string, text: string}}
+ */
+
+const todo = {
+  id: 1,
+  text: "Hello",
+};
